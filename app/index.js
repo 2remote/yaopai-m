413cd615f05ccba842c4bf6b33809cc77b8ce7f5
Peter Wong <amile1860@gmail.com>
2016-04-08 14:23:00 +0800

    添加router等内容

--- app/index.js
+++ app/index.js
@@ -1,16 +1,42 @@
+/* 1. 基本React引入 */
 import React from 'react';
 import ReactDOM from 'react-dom';
+/* 2. 路由相关 */
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+/* 3. 接入redux */
+import { Provider } from 'react-redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
 
-var App = React.createClass({
-  render(){
-    return (
-      <div className="container">
-        Welcome To Facebook
-      </div>
-    );
-  }
+let sampleReducer = combineReducers({
+  name: (name='John', action) => name,
+  routing: routerReducer
 });
 
+let store = createStore(sampleReducer);
+
+const history = syncHistoryWithStore(hashHistory, store);
+
+const Sample = props => (
+  <div>
+    <h1>Sample Container</h1>
+    {props.children}
+  </div>
+);
+
+const SampleContent = props => (
+  <div>Hello from sample content</div>
+);
+
+const App = () => (
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={Sample}>
+        <Route path="content" component={SampleContent} />
+      </Route>
+    </Router>
+  </Provider>
+);
 
 ReactDOM.render(
   <App/>,
