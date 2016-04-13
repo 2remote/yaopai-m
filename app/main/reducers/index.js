8709aaefd69444667c87f66dcd98a0c3f9f6d79f
Peter Wong <amile1860@hotmail.com>
2016-04-13 09:43:51 +0800

    根据ESLint调整reducer

--- app/main/reducers/index.js
+++ app/main/reducers/index.js
@@ -1,10 +1,8 @@
-import { combineReducers } from 'redux';
+// import { combineReducers } from 'redux';
 
 /**
  * 这个方法处理作品列表刷新
  */
-const workReducer = (state = [], action) => {
-  return state;
-};
+const workReducer = (state = []) => state;
 
 export default workReducer;
