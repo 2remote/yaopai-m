991e11ebf8dd775c71f4cab9de62b0fe637899ce
可乐 <zaxlct@foxmail.com>
2016-04-20 17:28:17 +0800

    route 激活时，可以接收传入的 className

--- app/main/layouts/MainLayout.js
+++ app/main/layouts/MainLayout.js
@@ -1,10 +1,6 @@
 import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
 
-const active = {
-  color: 'red',
-};
-
 const MainLayout = props => (
   <div>
     { props.children }
@@ -12,16 +8,16 @@ const MainLayout = props => (
       <hr />
       <div className="pure-g">
         <div className="pure-u-1-4" style={{ textAlign: 'center' }}>
-          <Link to="/main/work" activeStyle={active}>作品</Link>
+          <Link to="/main/work" activeClassName="active">作品</Link>
         </div>
         <div className="pure-u-1-4" style={{ textAlign: 'center' }}>
-          <Link to="/main/discovery" activeStyle={active}>发现</Link>
+          <Link to="/main/discovery" activeClassName="active">发现</Link>
         </div>
         <div className="pure-u-1-4" style={{ textAlign: 'center' }}>
-          <Link to="/main/grapher" activeStyle={active}>摄影师</Link>
+          <Link to="/main/grapher" activeClassName="active">摄影师</Link>
         </div>
         <div className="pure-u-1-4" style={{ textAlign: 'center' }}>
-          <Link to="/main/user" activeStyle={active}>我的</Link>
+          <Link to="/main/user" activeClassName="active">我的</Link>
         </div>
       </div>
     </nav>
