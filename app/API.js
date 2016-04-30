e8d1ea4161e2f7bb1e0543f893e1f65f7af6161d
可乐 <zaxlct@foxmail.com>
2016-04-30 18:06:57 +0800

    update api

--- app/API.js
+++ app/API.js
@@ -7,7 +7,9 @@ const DEV_ENV = '//dev.api.aiyaopai.com/';
 // TODO: 临时测试用，建议讨论一下数据的组织（url和Fields合并是不是可行？）
 const temp = `${DEV_ENV}?API=Albums.Search`;
 const tempGet = `${DEV_ENV}?API=Albums.Get`;
-const LOGIN = `${DEV_ENV}?API=User.Login`;
+const Login = `${DEV_ENV}?API=User.Login`;
+const SendTelRegister = `${DEV_ENV}?API=User.SendTelRegister`;       //   让后台发送手机注册验证码
+const ReceiveTelRegister = `${DEV_ENV}?API=User.ReceiveTelRegister`; // 后台接收手机注册验证码
 
 const API = {
   WORK: {
@@ -15,7 +17,9 @@ const API = {
     GET: tempGet,
   },
   USER: {
-    LOGIN,
+    Login,
+    SendTelRegister,
+    ReceiveTelRegister,
   },
 };
 
