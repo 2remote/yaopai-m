de1f3bb47f38e9ecacc23fd26876e46b111a23b2
可乐 <zaxlct@foxmail.com>
2016-05-01 15:00:19 +0800

    更改文件结尾
    jsx => js

--- app/user/main/layouts/SelectLayout.js
+++ app/user/main/layouts/SelectLayout.js
@@ -2,9 +2,13 @@ import React, { Component/* ,PropTypes*/ } from 'react';
 // import { Link } from 'react-router';
 
 class SelectLayout extends Component {
+  componentDidMount() {
+    // TODO
+  }
+
   render() {
     return (
-      <div>
+      <section>
         <ul className="select-panel">
           <li><a>拍摄地点<i className="down" /></a></li>
           <li><a>拍摄系列<i className="down" /></a></li>
@@ -13,7 +17,7 @@ class SelectLayout extends Component {
         <ul className="select-item-panel">
           <li></li>
         </ul>
-      </div>
+      </section>
     );
   }
 }
