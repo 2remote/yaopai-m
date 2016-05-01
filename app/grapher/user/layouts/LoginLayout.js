d2e83567ec82f008139f0f62e8cbc51531b9ee19
可乐 <zaxlct@foxmail.com>
2016-05-01 11:30:37 +0800

    Login / Register 布局

--- app/grapher/user/layouts/LoginLayout.js
+++ app/grapher/user/layouts/LoginLayout.js
@@ -3,25 +3,24 @@ import React from 'react';
 const LoginLayout = props => {
   let name;
   let password;
-  console.log(props);
+  const { user } = props;
   return (
-    <div>
+    <section>
       <form onSubmit={() => {
         props.onSubmit(name.value.trim(), password.value.trim());
       }}
       >
-        <input type="text" ref={node => name = node} />
-        <label>userName</label>
+        <input type="number" ref={node => name = node} />
+        <label>手机号</label>
         <br />
-        <input type="text" ref={node => password = node} />
-        <label>userPassword</label>
+        <input type="password" ref={node => password = node} />
+        <label>密码</label>
         <br />
-        <button type="submit">提交</button>
+        <button type="submit">登陆</button>
       </form>
 
-      <h1>{props.user.nickname}</h1>
-      <h1>{props.user.userSex}</h1>
-    </div>
+      <h1>用户昵称：{user.nickname}</h1>
+    </section>
   );
 };
 
