07e70ee536b2c7ac02862d62c8fcca1f2437a86f
可乐 <zaxlct@foxmail.com>
2016-05-03 09:23:15 +0800

    Simplify component

--- app/user/main/layouts/MainLayout.js
+++ app/user/main/layouts/MainLayout.js
@@ -2,15 +2,13 @@ import React, { PropTypes } from 'react';
 import SelectLayout from './SelectLayout';
 import MenuLayout from './MenuLayout';
 
-const MainLayout = props => {
-  return (
-    <div style={{ paddingTop: 50, position: 'relative' }}>
-      {props.children}
-      <SelectLayout />
-      <MenuLayout />
-    </div>
-  );
-};
+const MainLayout = props => (
+  <div style={{ paddingTop: 50, position: 'relative' }}>
+    {props.children}
+    <SelectLayout />
+    <MenuLayout />
+  </div>
+);
 
 MainLayout.propTypes = {
   children: PropTypes.element,
