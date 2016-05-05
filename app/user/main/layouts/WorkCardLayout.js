14be43abee89468e166ba921510ac4fba634e32c
可乐 <zaxlct@foxmail.com>
2016-05-05 15:26:33 +0800

    去掉边框

--- app/user/main/layouts/WorkCardLayout.js
+++ app/user/main/layouts/WorkCardLayout.js
@@ -1,7 +1,7 @@
 import React, { PropTypes } from 'react';
 
 const WorkCardLayout = ({ cover, title, price, views, nickname }) => (
-  <article style={{ border: '1px solid red', marginBottom: '3px' }}>
+  <article style={{ marginBottom: '3px' }}>
     <div><img style={{ width: '100%' }} src={ cover } /></div>
     <h3>{ title } - <small>{ nickname }</small></h3>
     <p>价格：{ price }</p>
