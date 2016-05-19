5febc8fa814377d753852ce3ac94867335ce0840
可乐 <zaxlct@foxmail.com>
2016-05-19 16:44:58 +0800

    增加 action 制造器、修改信息接口

--- app/grapher/user/actions.js
+++ app/grapher/user/actions.js
@@ -2,43 +2,40 @@ import API from 'app/API'
 import post from 'app/HttpFactory'
 import md5 from 'blueimp-md5'
 import base64encode from 'tool/base64'
-import { SAVE_USERINFO, SEND_TEL_REGISTER, RECEIVE_TEL_REGISTER, LOGIN_FAILED,
-  GET_PG_DATA } from './constant'
+import { SAVE_USERINFO, SEND_TEL_REGISTER, RECEIVE_TEL_REGISTER,
+  GET_PG_DATA, CHANGE_AVATAR, CHANGE_INFO } from './constant'
+
+// action 制造器
+const makeActionCreator = (type, ...argNames) => (...args) => {
+  const action = { type }
+  argNames.forEach((arg, index) => {
+    action[argNames[index]] = args[index]
+  })
+  return action
+}
 
 // 如果用户登陆成功
-const userLoginSuccessAction = (userData) => ({
-  type: SAVE_USERINFO,
-  userData,
-})
-
-// 如果用户登陆失败
-const userLoginFailedAction = (errorMsg) => ({
-  type: LOGIN_FAILED,
-  errorMsg,
-})
+const userLoginSuccessAction = makeActionCreator(SAVE_USERINFO, 'userData')
+// 相当于
+// const userLoginSuccessAction = (SAVE_USERINFO, userData) => ({
+//   type: SAVE_USERINFO,
+//   userData
+// })
 
 const loginPost = (loginname, sign, time, dispatch) => {
   post(API.USER.LoginWithSign, { loginname, sign, time }).then(data => {
-    if (data.Success) {
-      const userData = {
-        loginToken: data.LoginToken,
-        sessionToken: data.SessionToken,
-        userId: data.User.Id,
-        nickname: data.User.Name,
-        avatar: data.User.Avatar,
-        userSex: data.User.Sex,
-        userType: data.User.TypeString,
-        signature: data.User.Signature,
-      }
-      dispatch(userLoginSuccessAction(userData))
-    } else {
-      // 为什么这个 dispatch 没执行
-      dispatch(userLoginFailedAction(data.ErrorMsg))
-      throw data.ErrorMsg
+    const userData = {
+      loginToken: data.LoginToken,
+      sessionToken: data.SessionToken,
+      userId: data.User.Id,
+      nickname: data.User.Name,
+      avatar: data.User.Avatar,
+      userSex: data.User.Sex,
+      userType: data.User.TypeString,
+      signature: data.User.Signature,
     }
+    dispatch(userLoginSuccessAction(userData))
   }).catch(error => {
-    /* HACK: eslint-disable no-console */
-    /* eslint-disable no-console */
     console.error(error)
   })
 }
@@ -46,16 +43,10 @@ const loginPost = (loginname, sign, time, dispatch) => {
 
 export const userLoginActionAsync = (loginname, password) => dispatch => {
   post(API.USER.GetSysTime).then(data => {
-    if (data.Success) {
-      const serverTime = data.ServerTime
-      const sign = md5(base64encode(`${serverTime}${md5(password)}`))
-      loginPost(loginname, sign, serverTime, dispatch)
-    } else {
-      throw data.ErrorMsg
-    }
+    const serverTime = data.ServerTime
+    const sign = md5(base64encode(`${serverTime}${md5(password)}`))
+    loginPost(loginname, sign, serverTime, dispatch)
   }).catch(error => {
-    /* HACK: eslint-disable no-console */
-    /* eslint-disable no-console */
     console.error(error)
   })
 }
@@ -63,22 +54,13 @@ export const userLoginActionAsync = (loginname, password) => dispatch => {
 /* User.SendTelRegister 发送手机注册验证码
  * 把用户手机号提交给后台，让后台发验证码到该手机号上
  */
-const sendTelRegisterAction = (isSendTelSuccess) => ({
-  type: SEND_TEL_REGISTER,
-  isSendTelSuccess,
-})
+const sendTelRegisterAction = makeActionCreator(SEND_TEL_REGISTER, 'isSendTelSuccess')
 
 export const sendTelRegisterActionAsync = tel => dispatch => {
   post(API.USER.SendTelRegister, { tel }).then(data => {
-    if (data.Success) {
-      const isSendTelSuccess = data.Success
-      dispatch(sendTelRegisterAction(isSendTelSuccess))
-    } else {
-      throw data.ErrorMsg
-    }
+    const isSendTelSuccess = data.Success
+    dispatch(sendTelRegisterAction(isSendTelSuccess))
   }).catch(error => {
-    /* HACK: eslint-disable no-console */
-    /* eslint-disable no-console */
     console.error(error)
   })
 }
@@ -87,21 +69,14 @@ export const sendTelRegisterActionAsync = tel => dispatch => {
 /* User.ReceiveTelRegister 接收手机注册验证码
  * 验证用户输入的验证码是否正确
  */
-const receiveTelRegisterAction = (isReceiveTelSuccess) => ({
-  type: RECEIVE_TEL_REGISTER,
-  isReceiveTelSuccess,
-})
+const receiveTelRegisterAction = makeActionCreator(RECEIVE_TEL_REGISTER, 'isReceiveTelSuccess')
 
 export const receiveTelRegisterActionAsync = (tel, code, password) => dispatch => {
   const postData = { tel, code, password }
   post(API.USER.ReceiveTelRegister, postData).then(data => {
-    if (data.Success) {
-      const isReceiveTelSuccess = data.Success
-      dispatch(receiveTelRegisterAction(isReceiveTelSuccess))
-    }
+    const isReceiveTelSuccess = data.Success
+    dispatch(receiveTelRegisterAction(isReceiveTelSuccess))
   }).catch(error => {
-    /* HACK: eslint-disable no-console */
-    /* eslint-disable no-console */
     console.error(error)
   })
 }
@@ -110,21 +85,42 @@ export const receiveTelRegisterActionAsync = (tel, code, password) => dispatch =
 /*
  * USER.GetPgData 获取摄影师详情
  */
-const getPgDataAction = (pgData) => ({
-  type: GET_PG_DATA,
-  pgData,
-})
+const getPgDataAction = makeActionCreator(GET_PG_DATA, 'pgData')
 
 export const getPgDataActionAsycn = (Id) => dispatch => {
   const postData = {
     Id,
-    Fields: 'NickName,Sex,Avatar',
+    Fields: 'NickName,Sex,Avatar,CityId',
   }
-  post(API.USER.GetPgData, postData).then(data => {
-    if (data.Success) {
-      dispatch(getPgDataAction(data))
-    }
-  }).catch(error => {
+  post(API.USER.GetPgData, postData).then(data =>
+    dispatch(getPgDataAction(data))
+  ).catch(error => {
     console.error(error)
   })
+}
+
+
+/*
+ * User.ChangeInfo 修改登陆用户信息
+ */
+const isChangeInfoSuccessAction = makeActionCreator(CHANGE_INFO, 'isChangeInfoSuccess')
+
+export const changeInfoActionAsycn = Avatar => dispatch => {
+  const postData = { Avatar }
+  post(API.USER.UserChangeInfo, postData)
+     .then(data => dispatch(isChangeInfoSuccessAction(data.Success)))
+     .catch(error => console.error(error))
+}
+
+
+ /*
+  * User.ChangeAvatar 修改登陆用户头像
+  */
+const isChangeAvatarSuccessAction = makeActionCreator(CHANGE_AVATAR, 'isChangeAvatarSuccess')
+
+export const changeAvatarActionAsycn = (Nickname, Sex, Location) => dispatch => {
+  const postData = { Nickname, Sex, Location }
+  post(API.USER.UserChangeAvatar, postData)
+      .then(data => dispatch(isChangeAvatarSuccessAction(data.Success)))
+      .catch(error => console.error(error))
 }
