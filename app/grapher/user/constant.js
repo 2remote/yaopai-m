826a7b139e1dc4b3eab0fd403bbc4d385f332ce5
可乐 <zaxlct@foxmail.com>
2016-05-19 16:46:30 +0800

    增加常量

--- app/grapher/user/constant.js
+++ app/grapher/user/constant.js
@@ -1,5 +1,6 @@
 export const SAVE_USERINFO = 'SAVE_USERINFO'
 export const SEND_TEL_REGISTER = 'SEND_TEL_REGISTER'
 export const RECEIVE_TEL_REGISTER = 'RECEIVE_TEL_REGISTER'
-export const LOGIN_FAILED = 'LOGIN_FAILED'
-export const GET_PG_DATA = 'GET_PG_DATA'
+export const GET_PG_DATA = 'GET_PG_DATA' // 获取摄影师详情
+export const CHANGE_AVATAR = 'CHANGE_AVATAR' // 修改头像
+export const CHANGE_INFO = 'CHANGE_INFO'// 修改登陆用户信息
