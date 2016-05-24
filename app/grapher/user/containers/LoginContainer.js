913eb71c73f2b1a2036229a7a7c949cf04e07438
可乐 <zaxlct@foxmail.com>
2016-05-24 15:46:43 +0800

    更新 Immutable

--- app/grapher/user/containers/LoginContainer.js
+++ app/grapher/user/containers/LoginContainer.js
@@ -3,7 +3,7 @@ import LoginLayout from 'user/layouts/LoginLayout'
 import { userLoginActionAsync } from 'user/actions'
 
 const mapStateToProps = state => ({
-  user: state.user.login,
+  user: state.getIn(['user', 'login']),
 })
 
 const mapDispatchToProps = dispatch => ({
